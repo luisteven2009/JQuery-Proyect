93539d274b800fe4b1082ca61327ec3fb52a4532
luisteven2009 <luisteven2009@gmail.com>
2019-08-17 18:52:27 -0600

    Steven Update

--- js/form.js
+++ js/form.js
@@ -39,40 +39,11 @@ $(function() {
 		var username_length = $("#form_username").val().length;
 		
 		if(username_length < 5 || username_length > 20) {
-			$("#username_error_message").html("Should be between 5-20 characters");
+			$("#username_error_message").html("Deberia ser entre 5 y 20 caracteres");
 			$("#username_error_message").show();
 			error_username = true;
 		} else {
 			$("#username_error_message").hide();
-		}
-	
-	}
-
-	function check_password() {
-	
-		var password_length = $("#form_password").val().length;
-		
-		if(password_length < 8) {
-			$("#password_error_message").html("At least 8 characters");
-			$("#password_error_message").show();
-			error_password = true;
-		} else {
-			$("#password_error_message").hide();
-		}
-	
-	}
-
-	function check_retype_password() {
-	
-		var password = $("#form_password").val();
-		var retype_password = $("#form_retype_password").val();
-		
-		if(password !=  retype_password) {
-			$("#retype_password_error_message").html("Passwords don't match");
-			$("#retype_password_error_message").show();
-			error_retype_password = true;
-		} else {
-			$("#retype_password_error_message").hide();
 		}
 	
 	}
@@ -84,7 +55,7 @@ $(function() {
 		if(pattern.test($("#form_email").val())) {
 			$("#email_error_message").hide();
 		} else {
-			$("#email_error_message").html("Invalid email address");
+			$("#email_error_message").html("Dirección de correo invalido");
 			$("#email_error_message").show();
 			error_email = true;
 		}
